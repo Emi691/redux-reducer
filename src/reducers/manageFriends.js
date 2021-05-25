7d72f1609da9d8cab55f77fbf7b6c4ec4f1818b9
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-05-25 00:13:56 -0700

    commit tree 5710e823003494530f235b3ceb1f38017a550674

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+    const index = state.friends.findIndex(element => element.id === action.id)
+
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return {friends: [...state.friends, action.friend]}
+        case 'REMOVE_FRIEND':
+            return {friends: [...state.friends.slice(0, index), ...state.friends.slice(index + 1)]}
+        default:
+            return state
+    }
 }
